ac529d18d4ec3f1f68d75ba6d6c9c92ca8a00a68
10yilin <yilin.zhou.2018@sis.smu.edu.sg>
2020-11-12 21:49:01 +0800

    config webapp

---
+++ src/main/java/g1t3/controller/IndexController.java
@@ -0,0 +1,21 @@
+package g1t3.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+//    @GetMapping("/")
+//    public String index() {
+//        return "index";
+//    }
+
+    @GetMapping("/")
+    public String index() {
+        return "favourite";
+    }
+
+
+
+}
