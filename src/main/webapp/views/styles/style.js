77f1b61dd499711b9718700d228d294830b11470
jielinchew <54615492+jielinchew@users.noreply.github.com>
2020-11-14 22:58:17 +0800

    removing style.js

--- src/main/webapp/views/styles/style.js
+++
@@ -1,7 +0,0 @@
-$(document).load(function() {
-   var d = new Date();
-   d.setDate(d.getDate()+7);
-
-   $('#currentDate').datePick({dateFormat: 'dd-mm-yyyy', minDate: d});
-   var date1 = $('#currentDate').val();
-});
